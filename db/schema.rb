9e8f9d9aeea6f5f9a8d5ddc664bbf1205635c115
Amelie Petetin <amelie.petetin@gmail.com>
2018-03-04 23:21:58 +0100

    migrating users change to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304215242) do
+ActiveRecord::Schema.define(version: 20180304221955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
